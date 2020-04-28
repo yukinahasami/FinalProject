4d2e6bbb638c344e4a4137c616f45fbb41865a0b
Abby <Abby@192.168.0.22>
2020-04-28 09:53:58 -0400

    attempt

---
+++ FinalProject/src/covid19/SQLHelper.java
@@ -0,0 +1,14 @@
+/**
+ * CMPSC 221 Exercise *****#
+ * SQLHelper.java
+ * Purpose: ****
+ * 
+ * @author Abigail Burns
+ * @version: 1.0 ****Date
+ */
+package covid19;
+
+
+public class SQLHelper {
+
+}
